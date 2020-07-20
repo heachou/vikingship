4ab5ee7e5d3f2fbc38f0ea8d334367fa3a3e692f
heachou <1244239990@qq.com>
2020-07-20 18:04:33 +0800

    feat: button 组件雏形
    button 组件雏形

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
+import Button,{ ButtonType, ButtonSize }  from './components/button'
 
 function App() {
   return (
     <div className="App">
-      app
-      <code>
-        const a = n
-      </code>
+      <Button size={ButtonSize.Small} btnType={ButtonType.Danger}>hello</Button>
     </div>
   )
 }
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,3 +1,3 @@
 import Button from './button'
-
+export { ButtonType, ButtonSize } from './button'
 export default Button
